fcd2e6ab78011a09c6098486f69ec4c55ff0a3cb
BeAzarym <calvinchabeau01@gmail.com>
2023-05-03 10:28:45 +0200

    fix transparency

--- src/graphics/display_imgs.c
+++ src/graphics/display_imgs.c
@@ -6,11 +6,34 @@
 /*   By: cchabeau <cchabeau@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:07:10 by cchabeau          #+#    #+#             */
-/*   Updated: 2023/05/02 23:58:57 by cchabeau         ###   ########.fr       */
+/*   Updated: 2023/05/03 10:28:07 by cchabeau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/so_long.h"
+
+static void	load_coin_img(t_game *game, int x, int y)
+{
+	mlx_put_image_to_window(game->mlx, game->windows, game->img->floor, (y
+			* 16), (x * 16));
+	mlx_put_image_to_window(game->mlx, game->windows, game->img->coin, (y * 16),
+		(x * 16));
+}
+
+static void	load_player_img(t_game *game, int x, int y)
+{
+	if (game->player->x == game->map_data->exit
+		&& game->player->y == game->map_data->exit_y)
+		mlx_put_image_to_window(game->mlx, game->windows, game->img->player, (y
+				* 16), (x * 16));
+	else
+	{
+		mlx_put_image_to_window(game->mlx, game->windows, game->img->floor, (y
+				* 16), (x * 16));
+		mlx_put_image_to_window(game->mlx, game->windows, game->img->player, (y
+				* 16), (x * 16));
+	}
+}
 
 static void	load_img_win(t_game *game, int x, int y, char type)
 {
@@ -21,8 +44,7 @@ static void	load_img_win(t_game *game, int x, int y, char type)
 		mlx_put_image_to_window(game->mlx, game->windows, game->img->floor, (y
 				* 16), (x * 16));
 	if (type == 'P')
-		mlx_put_image_to_window(game->mlx, game->windows,
-			game->img->player, (y * 16), (x * 16));
+		load_player_img(game, x, y);
 	if (type == 'E')
 	{
 		if (game->player->coin == game->map_data->coin)
@@ -33,8 +55,7 @@ static void	load_img_win(t_game *game, int x, int y, char type)
 				game->img->exit_close, (y * 16), (x * 16));
 	}
 	if (type == 'C')
-		mlx_put_image_to_window(game->mlx, game->windows, game->img->coin, (y
-				* 16), (x * 16));
+		load_coin_img(game, x, y);
 }
 
 void	display_imgs(t_game *game)
